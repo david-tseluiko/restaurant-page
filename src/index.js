c56b9e83da6fbad68fbbbfe5c74c2c3ad02786ad
David Tseluiko <david.tseluiko@gmail.com>
2025-12-02 19:26:16 -0600

    Make the page working by making the navigation buttons work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import { createHomepage } from "./pages/home.js";
 import { createMenupage } from "./pages/menu.js";
 import { createContactpage } from "./pages/contact.js";
 
-// createHomepage();
-// createMenupage();
-createContactpage();
+const navigation = document.querySelector(".nav");
+const content = document.querySelector("#content");
+const pages = [createHomepage, createMenupage, createContactpage];
+
+createHomepage();
+
+navigation.addEventListener("click", (event) => {
+    const index = [...event.target.parentElement.children].indexOf(
+        event.target
+    );
+    const findPressedButton = navigation.children[index];
+
+    if (findPressedButton.classList.length < 2) {
+        for (let button of Array.from(navigation.children)) {
+            if (button.classList.length === 2) {
+                button.classList.remove("active");
+                break;
+            }
+        }
+
+        findPressedButton.classList.add("active");
+        content.innerHTML = "";
+        pages[index]();
+    }
+});
